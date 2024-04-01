4886bc6b3347bceb15a7741ac640363053576150
unknown <bencbchurchill@gmail.com>
2024-03-31 17:05:38 -0700

    Created test for TestDoDamageDriveEvaluator

--- tests/clarion_ext/test_motivation.py
+++ tests/clarion_ext/test_motivation.py
@@ -5,7 +5,7 @@ from pyClarion import nd
 import pytest
 
 from battlemaster.adapters.clarion_adapter import BattleConcept
-from battlemaster.clarion_ext.attention import GroupedChunk
+from battlemaster.clarion_ext.attention import GroupedChunk, GroupedChunkInstance
 from battlemaster.clarion_ext.motivation import (
     drive, DoDamageDriveEvaluator, KoOpponentDriveEvaluator, DriveStrength, GroupedStimulus
 )
@@ -52,3 +52,30 @@ class TestDriveStrength:
     def test_missing_drive_has_default_value(self, output):
         assert drive.DEBUFF_OPPONENT not in output
         assert output[drive.DEBUFF_OPPONENT] == output.default
+
+
+class TestDoDamageDriveEvaluator:
+    @pytest.fixture
+    def stimulus(self, request) -> GroupedStimulus:
+        is_force_switch = request.param
+        return {
+            BattleConcept.BATTLE: nd.NumDict({
+                GroupedChunkInstance('metadata', BattleConcept.BATTLE, [cl.feature('force_switch', is_force_switch)]): 1.
+            })
+        }
+
+    @pytest.fixture
+    def evaluator(self):
+        return DoDamageDriveEvaluator()
+
+    @pytest.mark.parametrize('stimulus', [True], indirect=True)
+    def test_evaluate_force_switch(self, evaluator: DoDamageDriveEvaluator, stimulus):
+        strength = evaluator.evaluate(stimulus)
+        assert strength == 0.5
+
+    @pytest.mark.parametrize('stimulus', [False], indirect=True)
+    def test_evaluate_not_force_switch(self, evaluator: DoDamageDriveEvaluator, stimulus):
+        strength = evaluator.evaluate(stimulus)
+        assert strength == 5.0
+
+# TODO test KoOpponentDriveEvaluator
